478748b6e5fa30f860cfe0233c3c83d6bfbf399c
Ubuntu <ubuntu@ip-172-31-46-82.ec2.internal>
2020-03-23 14:42:19 +0000

    add the ability to read and write messages and display it to the user

--- run.py
+++ run.py
@@ -1,22 +1,37 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
 
 
 @app.route("/")
 def index():
     """Main page with instructions"""
-    return "To send a meesage use /USERNAME/MESSAGE"
-    
+    return "To send a message use: /USERNAME/MESSAGE"
+
+
 @app.route("/<username>")
 def user(username):
-    return "Hi "+username
-@app.route("/<username>/<message>")
-def send_message(username,message):
-    return "{0}: {1}".format(username,message)
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
-if __name__ =="__main__":
-    app.run(host=os.getenv("IP"),
-       port=int(os.getenv("PORT")),
-       debug=True)
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
+
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
