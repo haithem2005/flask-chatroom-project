c3372b72f79fb6e528847ff78a0453704bed0796
Ubuntu <ubuntu@ip-172-31-46-82.ec2.internal>
2020-03-25 09:17:29 +0000

    cleaning and refactoring the code

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, render_template,request,session
+from flask import Flask, redirect, render_template,request,session, url_for
 from datetime import datetime
 
 app = Flask(__name__)
@@ -7,13 +7,10 @@ app.secret_key="randomstring123"
 messages = []
 
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the `messages` list"""
     now=datetime.now().strftime("%H:%M:%S")
-    messages_dict = {"timestamp":now, "from":username, "message":message}
-    messages.append(messages_dict)
-
-
+    messages.append({"timestamp":now, "from":username, "message":message})
 
 
 
@@ -23,25 +20,20 @@ def index():
     if request.method=="POST":
         session["username"]=request.form["username"]
     if "username" in session:
-        return redirect(session["username"])
+        return redirect(url_for("user",username=session["username"]))
     return render_template("index.html")
 
 
-@app.route("/<username>", methods=["GET","POST"])
+@app.route("/chat/<username>", methods=["GET","POST"])
 def user(username):
-    """Display chat messages"""
+    """Add and  Display chat messages"""
     if request.method=="POST":
         username=session["username"]
         message=request.form["message"]
-        add_messages(username,message)
-        return redirect(session["username"]) 
+        add_message(username,message)
+        return redirect(url_for("user",username=session["username"])) 
     return render_template("chat.html", username=username, chat_messages=messages)
 
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
-    add_messages(username, message)
-    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
